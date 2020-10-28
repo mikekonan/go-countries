ac3687ece327f84a656c4a1dbaeaf51dfd4ac25f
Mikalai Konan <mikalai.konan@icloud.com>
2020-10-28 15:30:00 +0300

    npm script

--- app.js
+++ app.js
@@ -1,28 +1,26 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const buff = fs.readFileSync(0);
 
 const codes = JSON.parse(buff.toString());
 
-const countryByCountry = codes.map(country =>
-    `"${country["English short name"]}": {
-		country: "${country["English short name"]}",
-		alpha2:  "${country["Alpha-2 code"]}",
-		alpha3:  "${country["Alpha-3 code"]}",
+const countryByCountry =
+    codes.map(country => `"${country['English short name']}": {
+		country: "${country['English short name']}",
+		alpha2:  "${country['Alpha-2 code']}",
+		alpha3:  "${country['Alpha-3 code']}",
     }`);
 
-const countryByAlpha2 = codes.map(country =>
-	`"${country["Alpha-2 code"]}": {
-		country: "${country["English short name"]}",
-		alpha2:  "${country["Alpha-2 code"]}",
-		alpha3:  "${country["Alpha-3 code"]}",
+const countryByAlpha2 = codes.map(country => `"${country['Alpha-2 code']}": {
+		country: "${country['English short name']}",
+		alpha2:  "${country['Alpha-2 code']}",
+		alpha3:  "${country['Alpha-3 code']}",
     }`);
 
-const countryByAlpha3 = codes.map(country =>
-	`"${country["Alpha-3 code"]}": {
-		country: "${country["English short name"]}",
-		alpha2:  "${country["Alpha-2 code"]}",
-		alpha3:  "${country["Alpha-3 code"]}",
+const countryByAlpha3 = codes.map(country => `"${country['Alpha-3 code']}": {
+		country: "${country['English short name']}",
+		alpha2:  "${country['Alpha-2 code']}",
+		alpha3:  "${country['Alpha-3 code']}",
     }`);
 
 let template = `// This code is auto-generated; DO NOT EDIT.
